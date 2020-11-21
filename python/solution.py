78af3050095a5961eb6b96e039658e4674e0e7f6
todd <thivnor@gmail.com>
2020-11-21 10:20:16 -0800

    --

--- python/solution.py
+++ python/solution.py
@@ -10,6 +10,7 @@ def make_diffs_terse(given: list) -> list:
     return [i-given[i] for i in range(len(given))]
 
 def make_diffs_readable(given: list) -> list:
+    # This does the same thing make_diffs_terse but is more readable.
     result = []
     for index, value in enumerate(given):
         result.append(index-value)
